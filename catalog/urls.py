9f7e6572f98ca2d61f5763ee1aa14c1130d20090
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-06-21 18:28:01 +0300

    add new controller

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home
 from catalog.views import contacts
+from catalog.views import products_list
+from catalog.views import product_detail
 
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('contacts/', contacts, name='contacts'),
+    path('products/', products_list, name='products_list'),
+    path('product/', product_detail, name='product_detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
 def home(request):
@@ -7,3 +8,15 @@ def home(request):
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'products_list.html', context)
+
+
+def product_detail(request, pk):
+    product = Product.objects.all()
+    context = {"product": product}
+    return render(request, 'product_detail.html', pk)
