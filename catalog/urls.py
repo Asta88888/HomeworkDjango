d0dae5bcf6c5594d54ba9ce68c8ed42986e78d4f
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-06-28 15:31:06 +0300

    end to crud for product

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,9 +8,9 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
-    path('contacts/', contacts, name='contacts'),
+    path('catalog/contacts/', contacts, name='contacts'),
     path('catalog/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
     path('catalog/create', ProductCreateView.as_view(), name='product_create'),
-    path('catalog/<int:pk>/update', ProductUpdateView.as_view(), name='product_update'),
-    path('catalog/<int:pk>/delete', ProductDeleteView.as_view(), name='product_delete')
+    path('catalog/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('catalog/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete')
 ]
