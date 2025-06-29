a598819fa62e413cde64b8f6a0d1c5025f768a55
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-06-29 13:36:14 +0300

    add controllers and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from blog.models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content', 'preview']
+    success_url = reverse_lazy('blog:post_list')
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content', 'preview']
+    success_url = reverse_lazy('blog:post_list')
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:post_list')
