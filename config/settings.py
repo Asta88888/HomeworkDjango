aded4b9b3d9b248ac71048faf5c9c90a2f7e9772
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-07-09 12:10:04 +0300

    add send mail

--- config/settings.py
+++ config/settings.py
@@ -101,3 +101,12 @@ AUTH_USER_MODEL = 'users.User'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'asta.soul@yandex.ru'
+EMAIL_HOST_PASSWORD = 'qgidayfrfdqbpvkh'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
