bf4851fe0a795a00e70388fa58cb6d3cf3a7dc7f
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-07-09 04:09:43 +0300

    add and fixed templates

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,21 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from users.models import User
 
 
-class UserForm(UserCreationForm):
+class UserRegisterForm(UserCreationForm):
+    phone = forms.CharField(max_length=35, required=False)
+    username = forms.CharField(max_length=50, required=True)
+
+
     class Meta:
         model = User
-        fields = ('email', 'password1', 'password2')
+        fields = ('email', 'username', 'password1', 'password2')
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone_number')
+        if phone and not phone.isdigit():
+            raise forms.ValidationError('Номер телефона должен состоять только из цифр')
+        return phone
+
 
