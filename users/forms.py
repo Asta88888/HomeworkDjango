650c53ee3de1eb3dd5fd1c30ded2d25e4dd22ec8
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-07-09 11:44:34 +0300

    fixed logout and register url

--- users/forms.py
+++ users/forms.py
@@ -5,15 +5,15 @@ from users.models import User
 
 class UserRegisterForm(UserCreationForm):
     phone = forms.CharField(max_length=35, required=False)
-    username = forms.CharField(max_length=50, required=True)
+    usable_password = None
 
 
     class Meta:
         model = User
-        fields = ('email', 'username', 'password1', 'password2')
+        fields = ('email', 'password1', 'password2', 'phone')
 
     def clean_phone(self):
-        phone = self.cleaned_data.get('phone_number')
+        phone = self.cleaned_data.get('phone')
         if phone and not phone.isdigit():
             raise forms.ValidationError('Номер телефона должен состоять только из цифр')
         return phone
--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='catalog:catalog_list'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='catalog:product_list'), name='logout'),
 
 ]
 
